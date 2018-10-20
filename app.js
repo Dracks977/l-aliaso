98ce9f44a797b2bf057389634d58133409a04905
Dracks <eclat.nathan@gmail.com>
2018-10-20 02:37:18 +0200

    first

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const http = require('http').Server(app);
+const httpd = require('https');
+const fs = require('fs');
+const bodyParser = require('body-parser')
+const ejs = require('ejs');
+const path = require('path');
+const session = require('express-session');
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('database', 'username', 'password', {
+  host: 'localhost',
+  dialect: 'mysql',
+  operatorsAliases: false,
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+});
