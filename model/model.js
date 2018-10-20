3795f80b89129f90298d2be544023247e9fbbfc5
Dracks <eclat.nathan@gmail.com>
2018-10-21 00:13:19 +0200

    up

--- model/model.js
+++ model/model.js
@@ -1,47 +1,47 @@
 module.exports = (sequelize, Sequelize) => {
 	const Verbes = sequelize.define('verbes', {
 		'1': {
-			type: Sequelize.STRING,
+			type: Sequelize.STRING(100),
 			allowNull: false,
 		},
 		'2': {
-			type: Sequelize.STRING,
+			type: Sequelize.STRING(100),
 			allowNull: false,
 		},
 		'3': {
-			type: Sequelize.STRING,
+			type: Sequelize.STRING(100),
 			allowNull: false,
 		},
 		'4': {
-			type: Sequelize.STRING,
+			type: Sequelize.STRING(100),
 			allowNull: false,
 		},
 		'5': {
-			type: Sequelize.STRING,
+			type: Sequelize.STRING(100),
 			allowNull: false,
 		},
 		'6': {
-			type: Sequelize.STRING,
+			type: Sequelize.STRING(100),
 			allowNull: false,
 		},
 		'7': {
-			type: Sequelize.STRING,
+			type: Sequelize.STRING(100),
 			allowNull: false,
 		},
 		'8': {
-			type: Sequelize.STRING,
+			type: Sequelize.STRING(100),
 			allowNull: false,
 		}
 	});
 
 	const Mots = sequelize.define('mots', {
 		fr: {
-			type: Sequelize.STRING,
+			type: Sequelize.STRING(100),
 			allowNull: false,
 			unique: true
 		},
 		al: {
-			type: Sequelize.STRING,
+			type: Sequelize.STRING(100),
 			allowNull: false,
 			unique: true
 		},
